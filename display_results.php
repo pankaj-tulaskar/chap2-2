ca15f67f7b36c1791f7134ac751ef608af216a81
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-02-13 19:30:22 -0500

    inserted code to display date on result page

--- display_results.php
+++ display_results.php
@@ -67,6 +67,8 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
-    </main>
+   	
+	<p>This calculation was done on <?php echo date('m/d/Y'); ?>.</p>
+   </main>
 </body>
 </html>
